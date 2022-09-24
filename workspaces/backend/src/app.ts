fbb2f635e3ad0e55c54932eb397b7a24f8cfe1dc
Theo Selin <theo.selin@hotmail.com>
2022-09-24 11:37:59 +0200

    working message interface with backend mess route

--- workspaces/backend/src/app.ts
+++ workspaces/backend/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, json, Request, Response } from 'express'
+import Message from '@chatapp/shared'
 import cors from 'cors'
 
 const app: Application = express()
@@ -6,8 +7,12 @@ app.use(cors())
 app.use(json())
 const port: number = parseInt(process.env.SERVER_PORT || "3001")
 
-app.get('/hello', (req: Request, res: Response) => {
-res.send('Hello, World!')
+app.get('/messages', (req: Request, res: Response<Message>) => {
+res.send({
+    id: "123",
+    text: "Hej",
+    timeStamp: new Date()
+})
 })
 
 app.listen(port, function () {
